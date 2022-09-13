2f7006738deb2c8a888b5f1d0656f40f4796aa05
Amanda Bienz <bienz@amandasworkmbp.unm.edu>
2022-09-13 12:50:02 -0600

    Added unoptimized C++ code to do matrix-matrix multiplication

---
+++ matmat_orig.cpp
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <cmath>
+#include "timer.h"
+
+// To compile with and without vectorization (in gcc):
+// g++ -o matmat_orig matmat_orig.cpp -O1     <--- no vectorization
+// Flag to vectoize : -ftree-vectorize  
+// Flag needed for vectorization of X86 processors : -msse -msse2
+// Flag needed for vectorization of PowerPC platforms : -maltivec
+// Other optional flags (floating point reductions) : -ffast-math -fassociative-math
+//
+// To see what the compiler vectorizes : -fopt-info-vec (or -fopt-info-vec-optimized)
+// To see what the compiler is not able to vectorize : -fopt-info-vec-missed
+
+
+// Matrix-Matrix Multiplication of Doubles (Double Pointer)
+// Test without the restrict variables
+void matmat(int n, double* A, double*  B, double* C, int n_iter)
+{
+    double val;
+    for (int iter = 0; iter < n_iter; iter++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int k = 0; k < n; k++)
+                C[i*n+k] = 0;
+
+            for (int j = 0; j < n; j++)
+            {
+                val = A[i*n+j];
+                for (int k = 0; k < n; k++)
+                {
+                    C[i*n+k] += val * B[j*n+k];
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+
+    double start, end;
+    int n_access = 1000000000;
+    
+    if (argc == 1)
+    {
+        printf("Pass Matrix Dimension n as Command Line Arg\n");
+        return 0;
+    }
+
+    int n = atoi(argv[1]);
+    int n_iter = n_access / (n*n*n);
+
+    double* A = (double*)malloc(n*n*sizeof(double));
+    double* B = (double*)malloc(n*n*sizeof(double));
+    double* C = (double*)malloc(n*n*sizeof(double));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            A[i*n+j] = 1.0/(i+1);
+            B[i*n+j] = 1.0;
+        }
+    }
+
+    // Warm-Up 
+    matmat(n, A, B, C, n_iter);
+
+    start = get_time();
+    matmat(n, A, B, C, n_iter);
+    end = get_time();
+    printf("N %d, Time Per MatMat %e\n", n, (end - start)/n_iter);
+
+    free(A);
+    free(B);
+    free(C);
+        
+
+    return 0;
+}
